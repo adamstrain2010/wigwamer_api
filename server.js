54d124e141d12f22f64b20f69c7e23900a99326b
Adam Strain <adamstrain2010@gmail.com>
2017-09-24 21:53:08 +0000

    *ADDED ROOM TYPE ROUTE

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ var wwServerConfig = {
 	password: "W1gw@m3r!",
 	server: "wigwamer.com",
 	database: "pms"
-}
+};
+
 //FUNCTION TO CONNECT TO DB AND EXEC QUERY 
 var executeQuery = function(res, query){
      sql.close();
@@ -86,6 +87,11 @@ var executeWWQuery = function(res, query){
     })
 }
 
+app.get("/api/getSystemDate", function(req, res){
+	var query = "select top 1 systemdate from systemdates";
+	console.log(query);
+	executeWWQuery(res, query);
+});
 
 app.get("/api/testing",function(req,res){
 	var query = 'select * from "user"';
@@ -108,69 +114,99 @@ app.get("/api/search", function(req,res){
 	executeQuery(res, query);
 });
 
+//DATE
+	//GET SYSTEM DATE
+app.get("/api/getSystemDate", function(req,res){
+	var query = "SELECT systemdate FROM systemdates";
+	console.log(query);
+	executeWWQuery(res, query);
+})
+
+//APP
+	//GET ROOM TYPES
+app.get("/api/getRoomTypes", function(req, res){
+	var query = "SELECT * FROM unittype WHERE inuse = 'Y' AND idproperty = " + req.query.organisationId;
+	console.log(query);
+	executeWWQuery(res, query);
+})
+
 //!!POST API !!
 
 //USERS
+// app.post("/api/checkUser", function(req,res){
+	// console.log("SELECT * FROM users WHERE username='" + req.query.username + "' AND organisation = " + req.query.organisation + " AND password = '" + req.query.password + "'");
+	// //var query = "SELECT userid FROM users WHERE username='" + req.query.username + "' AND organisation = " + req.query.organisation + " AND password = '" + req.query.password + "'";
+	// var query = "SELECT * FROM users WHERE username='" + req.query.username + "' AND organisation = " + req.query.organisation + " AND password = '" + req.query.password + "'";
+	// executeQuery(res,query);
+// });
+
 app.post("/api/checkUser", function(req,res){
-	console.log("SELECT * FROM users WHERE username='" + req.query.username + "' AND organisation = " + req.query.organisation + " AND password = '" + req.query.password + "'");
 	//var query = "SELECT userid FROM users WHERE username='" + req.query.username + "' AND organisation = " + req.query.organisation + " AND password = '" + req.query.password + "'";
-	var query = "SELECT * FROM users WHERE username='" + req.query.username + "' AND organisation = " + req.query.organisation + " AND password = '" + req.query.password + "'";
-	executeQuery(res,query);
+	var query = 'SELECT * FROM "user"' + " WHERE usercode = '" + req.query.username + "' AND idclient = " + req.query.organisation + " AND password = '"	+ req.query.password + "'";
+	console.log(query);
+	executeWWQuery(res,query);
 });
 
 //RESERVATIONS
 	//GET RESERVATIONS BY ARRIVALDATE
 app.post("/api/reservations", function(req,res){
-        console.log("SELECT * FROM reservations WHERE arrivalDate = '" + req.query.arrivalDate +  "' AND reservationStatusId = 1");
-        var query = "SELECT * FROM reservations r JOIN bookingSource bs on r.bookingSource = bs.bookingSOurceId WHERE arrivalDate = '" + req.query.arrivalDate + "' AND reservationStatusId = 1";
-        executeQuery(res,query);
+        var query = "SELECT r.idreservation, r.reservationname, r.fromdate, r.todate, rs.reservationsourcecode,rs.reservationcolour FROM reservations r JOIN reservationsource rs ON r.idreservationsource = rs.idreservationsource WHERE fromdate = '" + req.query.arrivalDate + "' AND idreservationstatus = 1"; 
+        console.log(query);
+		executeWWQuery(res,query);
+});
+
+//FOR DEV TURN ALL RESERVATIONS TO RESERVATION STATUS
+app.post("/api/changeAllToReservation", function(req,res){
+        var query = "UPDATE reservations SET idreservationstatus = 1"; 
+        console.log(query);
+		executeWWQuery(res,query);
 });
 
 //GET DEPARTING RESERVATIONS
 	//using 4 as checked in status
 app.post("/api/reservationsByDepartDate", function(req,res){
-		console.log("SELECT * FROM reservations WHERE departureDate = '" + req.query.departDate + "' AND reservationStatusId = 4");
-		var query = "SELECT * FROM reservations WHERE departureDate = '" + req.query.departDate + "' AND reservationStatusId = 4";
+		var query = "SELECT r.idreservation, r.reservationname, r.fromdate, r.todate, rs.reservationsourcecode FROM reservations r JOIN reservationsource rs ON r.idreservationsource = rs.idreservationsource WHERE todate = '" + req.query.departDate + "' AND idreservationstatus = 4";
 		console.log(query);
-		executeQuery(res, query);
+		executeWWQuery(res, query);
 })
-
-//GET SPECIFIC RESERVATION BY RESERVATIONNUM
-app.post("/api/getReservation", function(req,res){
-	console.log("SELECT * FROM reservations WHERE reservationId = " + req.query.reservationId);
-	var query = "SELECT * FROM reservations WHERE reservationId = " + req.query.reservationId;
-	executeQuery(res,query);
-});
 
 //GET IN HOUSE RESERVATIONS
 app.post("/api/reservationsInHouse", function(req,res){
-	console.log("SELECT * FROM reservations WHERE reservationStatusId = 4");
-	var query = "SELECT * FROM reservations WHERE reservationStatusId = 4";
-	executeQuery(res, query)
+	var query = "SELECT * FROM reservations r JOIN reservationsource rs ON r.idreservationsource = rs.idreservationsource WHERE idreservationstatus = 4";
+	console.log(query);
+	executeWWQuery(res, query)
 });
 
-app.post("/api/createReservation",function(req,res){
-	console.log("INSERT INTO reservations (surname,forename, reservationName, arrivalDate, departureDate, bookingSource, reservationStatusId) VALUES ('dylan', 'bob', 'dylan, bob' ,'2017-11-01', '2017-11-01', 2, 1)");
-	var query = "INSERT INTO reservations (surname,forename, reservationName, arrivalDate, departureDate, bookingSource, reservationStatusId) VALUES ('dylan', 'bob', 'dylan, bob' ,'2017-11-01', '2017-11-02', 2, 1)";
-	executeQuery(res, query);
+//GET SPECIFIC RESERVATION BY RESERVATIONNUM
+app.post("/api/getReservation", function(req,res){
+	var query = "SELECT r.*, g.* FROM reservations r join reservationsguest rg on r.idreservation = rg.idreservation join guest g on rg.idguest = g.idguest WHERE r.idreservation = " + req.query.reservationId;
+	console.log(query);
+	executeWWQuery(res,query);
 });
 
 app.post("/api/checkIn", function(req,res){
-	var query = "UPDATE reservations SET reservationStatusId = 4 WHERE reservationId = " + req.query.reservationNum;
-	executeQuery(res, query);
+	var query = "UPDATE reservations SET idreservationstatus = 4 WHERE idreservation = " + req.query.reservationNum;
+	console.log(query);
+	executeWWQuery(res, query);
 });
 
 app.post("/api/checkOut", function(req,res){
-	var query = "UPDATE reservations SET reservationStatusId = 5 WHERE reservationId = " + req.query.reservationNum;
-	executeQuery(res, query);
+	var query = "UPDATE reservations SET idreservationstatus = 5 WHERE idreservation = " + req.query.reservationNum;
+	console.log(query);
+	executeWWQuery(res, query);
 });
 
 app.post("/api/cancelReservation", function(req,res){
-	var query = "UPDATE reservations SET reservationStatusId = 2 WHERE reservationId = " + req.query.reservationNum;
+	var query = "UPDATE reservations SET idreservationstatus = 2 WHERE idreservation = " + req.query.reservationNum;
 	console.log(query);
-	executeQuery(res, query);
+	executeWWQuery(res, query);
 });
 
+app.post("/api/cancelAll", function(req,res){
+	var query = "UPDATE reservations SET idreservationstatus = 2";
+	console.log(query);
+	executeWWQuery(res, query);
+});
 //PUT API
 
 //NEW RESERVATION
@@ -197,25 +233,23 @@ app.post("/api/cancelReservation", function(req,res){
 // }
 
 // test();
-
 app.post("/api/saveReservation", function(req,res){
-	// var query = "INSERT INTO reservations (surname,forename,reservationName, arrivalDate, departureDate, bookingSource, reservationStatusId) VALUES ('" + req.query.surname + "','" + req.query.forename + "','" + req.query.surname + "," + req.query.forename + "','" + req.query.arrivalDate + "','" + req.query.departureDate + "'," + "1,1)";
 	query = "EXEC pms..sp_InsertReservation '" + req.query.surname + "','" + req.query.forename + "', '" + req.query.arrivalDate + "','" + req.query.departureDate + "', 1, 1";
-	// console.log(query);
-	// executeQuery(res,req);
-	var conn = new sql.ConnectionPool(dbConfig);
+	var conn = new sql.ConnectionPool(wwServerConfig);
 	conn.connect().then(function(conn){
 		var request = new sql.Request(conn);
+		//request.input('clientId', sql.Int, req.query.clientId);
 		request.input('surname', sql.NVarChar(50), req.query.surname);
 		request.input('forename', sql.NVarChar(50), req.query.forename);
-		request.input('arrivalDate', sql.NVarChar(50), req.query.arrivalDate);
-		request.input('departureDate', sql.NVarChar(50), req.query.departureDate);
-		request.input('bookingSource', sql.Int, req.query.bookingSource);
-		request.input('reservationStatus', sql.Int, 1);
-		request.execute('sp_InsertReservation')
-		.then(function(err, recordsets, returnValue, affected){
-			console.log(recordsets);
-			res.send(err);
+		request.input('arrivaldate', sql.NVarChar(50), req.query.arrivalDate);
+		request.input('departdate', sql.NVarChar(50), req.query.departureDate);
+		request.input('idreservationsource', sql.Int, req.query.bookingSource);
+		//request.input('reservationStatus', sql.Int, 1);
+		request.output('outReservationId', sql.Int);
+		request.output('outGuestId', sql.Int);
+		request.execute('sp_insertReservationGuest',function(err, recordsets, returnValue, affected){
+			console.dir(recordsets);
+			res.send(recordsets.output);
 		})
 		.catch(function(err){
 			res.send(err);
@@ -251,3 +285,18 @@ app.put("/api/saveReservation", function(req,res){
 	executeQuery(res, req); 
 });
 
+//ROLL DAY
+app.post("/api/rollDay", function(req,res){
+	var conn = new sql.ConnectionPool(wwServerConfig);
+	conn.connect().then(function(conn){
+		var request = new sql.Request(conn);
+		request.output('outDate', sql.NVarChar(50));
+		request.execute('sp_rollDay', function(err, recordsets, returnValue, affected){
+			res.send(recordsets.output);
+		})
+		.catch(function(err){
+			res.send(err);
+			console.log(err);
+		});
+	})
+});
